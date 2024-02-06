0190f3bc36f24bd6b113c4f417b9e5ab779ff793
Feroxpro <feliperamarcades@gmail.com>
2024-02-05 21:24:06 -0300

    Initial Commit

---
+++ DictionMaster/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DictionMaster
+//
+//  Created by Felipe Henrique Domingos on 05/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
